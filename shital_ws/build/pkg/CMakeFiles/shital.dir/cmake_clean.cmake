95771f4a00f8f5796d519ce9d27b60d28f17717b
Kokane Shital Namdev <shitalkokane45@gmail.com>
2025-07-10 10:42:19 +0530

    added shital wrkspce

---
+++ shital_ws/build/pkg/CMakeFiles/shital.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shital.dir/src/shital.cpp.o"
+  "CMakeFiles/shital.dir/src/shital.cpp.o.d"
+  "shital"
+  "shital.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shital.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ shital_ws/build/pkg/CMakeFiles/shital.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shital.
+# This may be replaced when dependencies are built.
